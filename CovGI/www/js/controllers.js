c624fd777d9c7414914e46f960a4f27f31692b9d
Osmolax <medaminebel.mab@gmail.com>
2017-04-12 17:01:11 +0100

    Update map

--- CovGI/www/js/controllers.js
+++ CovGI/www/js/controllers.js
@@ -94,42 +94,73 @@ angular.module('starter.controllers', [])
 
     })
 
-.controller('StaticCtrl', function ($scope) {
+.controller('StaticCtrl', function ($scope, $rootScope) {
     $scope.countries = {
         'Salé' : {
             'Salé Ville' : ['CAFE RAMSIS DIAR', 'BAB SABTA', 'BAB CHAAFA', 'SOUNBOULA'],
             'Hay Chmaou/Said Hajji' : ['CENTRAL LAITIERE', 'CAFE ZAYTOUNA','COCA COLA','CAFE IMANE','T13','CAFE IHSANE'],
-            'Hay Rahma' : ['CAFE HILTON', 'SUPER']
+            'Hay Rahma' : ['CAFE HILTON', 'SUPER'],
+            'Tabriket' : ['PHARMACIE IBTISSAM','BMCI','POSTE TABRIQUET','PHARMACIE DAHAB','GARE SALE TABRIKET','RESIDENCE NAKHIL'],
+            'Hay Karima' : ['CAFE BOUCHRA'],
+            'DOUAR CHEKH MFADAL' :['EN FACE DE DOUAR CHEKH MFADAL'],
+            'HAY SALAM' : ['TIJARI WAFA BANQUE','PETIT PRINCE A COTER DE LA POSTE'],
+            'Hay Inbiat' : ['terminus 14 cafe la lugua'],
+            'La base' : ['EN FACE DE LA BASE'],
+            'Rostal' : ['CAFE SOUK SALAM', 'PORTE RESIDENCE DOHA'],
+            'Bettana' : ['TILIWIN','LYCEE OMAR IBN KHATAB','CONSERVATION FONCIERE','ECOLE ALAMA'],
+            'Karia' : ['LYCEE IBN HASSAN WAZANI','BAB KARIA','METRO','CAFE HAJAR']
         }
     };
+
+
+
 })
 
-.controller('MapsCtrl', function($scope, $ionicLoading) {
+.controller('MapsCtrl', function($scope, $ionicLoading, $rootScope) {
+
 
 
     google.maps.event.addDomListener(window, 'load', function() {
 
-        var LatLingTechnopolice = {lat: 33.981979, lng: -6.726336};
-        navigator.geolocation.getCurrentPosition(function(pos) {
-            //33.993207, -6.721752
+
+            var directionsService = new google.maps.DirectionsService;
+            var directionsDisplay = new google.maps.DirectionsRenderer;
+
             var latLng = new google.maps.LatLng(33.993207,-6.721752);
-            var marker = new google.maps.Marker({
-                position: LatLingTechnopolice
-            });
+
             var mapOptions = {
                 center: latLng,
                 zoom: 14,
                 mapTypeId: google.maps.MapTypeId.ROADMAP
             };
+
             var map = new google.maps.Map(document.getElementById("map"), mapOptions);
-            var directionsService = new google.maps.DirectionsService;
-            var directionsDisplay = new google.maps.DirectionsRenderer;
-            //directionsDisplay.setMap(map);
-            DisplayRoute(directionsService, directionsDisplay);
+
+            directionsDisplay.setMap(map);
+
+            var LatSelectedPlace;
+            var LongSelectedPlace;
+
+            var onChangePlace = function () {
+                if( document.getElementById('suburb').value.equal('PETIT PRINCE A COTER DE LA POSTE')){
+                    LatSelectedPlace = 34.042083;
+                    LongSelectedPlace = -6.793549;
+                }else {
+                    //33.993005, -6.882308
+                   console.log(document.getElementById('suburb').value);
+                    LatSelectedPlace = 33.993005;
+                    LongSelectedPlace = -6.882308;
+                }
+                DisplayRoute(directionsService,directionsDisplay);
+            };
+
+
+            document.getElementById('suburb').addEventListener('change', onChangePlace);
+
             function DisplayRoute(directionsService,directionsDisplay) {
                 directionsService.route({
-                    origin: 'kenitra',
-                    destination: 'rabat',
+                    origin: new google.maps.LatLng(33.981979,-6.726336),
+                    destination: new google.maps.LatLng(LatSelectedPlace,LongSelectedPlace),
                     travelMode: 'DRIVING'
                 },function (response, status) {
                     if (status == 'OK'){
@@ -139,9 +170,16 @@ angular.module('starter.controllers', [])
                     }
                 });
             }
+
+            //var LatLingTechnopolice = {lat: 33.981979, lng: -6.726336};
+
+            /*var marker = new google.maps.Marker({
+                position: LatLingTechnopolice
+            });*/
+
             //marker.setMap(map);
             $scope.map = map;
-        });
+
     })
 })
 
